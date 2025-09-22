37e029b7e2b49fdb093c97cffbf3bb7af441d18f
Vikash Kumar <vikashsigh24@gmail.com>
2025-09-22 12:31:23 +0530

    Initial Commits

--- src/main/java/com/myapp/restapi/Controllers/UserController.java
+++ src/main/java/com/myapp/restapi/Controllers/UserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.myapp.restapi.Entities.Users;
 import com.myapp.restapi.Services.UserService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class UserController {
 
